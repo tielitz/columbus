52656f604d37b18a7d58e8bebd4d70942ba1e92b
Andreas Tielitz <tielitz@thr3.de>
2017-02-28 12:57:18 +0100

    Added a json loader for webpack to handle dependencies with a json entry point

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,15 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
+                exclude: /node_modules/,
                 loader: 'babel', // 'babel-loader' is also a legal name to reference
                 query: {
                     presets: ['es2015']
                 }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json',
             },
             {
                 test: /\.html$/,
